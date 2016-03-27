eadece46a86f8ad833a8995ecb3b50c15783ece7
Dick Davis <rdavis89@my.apsu.edu>
2016-03-26 23:37:02 -0500

    started q1

---
+++ q1/index_process.php
@@ -0,0 +1,48 @@
+<!--
+     Richard Davis
+     CSCI4000
+     24 Mar 2016
+-->
+<!DOCTYPE html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Richard Davis's Employee Net Pay</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Richard Davis's Employee Net Pay</h1>
+    </header>
+    <section>
+      <h3>This page calculates an employee's net pay.</h3>
+      <h4>You entered:</h4>
+      <div id="descriptions">
+        <p>Employee's hourly rate of pay =</p>
+        <p>Number of hours worked that week =</p>
+        <p>Number of dependents employee has =</p>
+        <p>Number of overtime hours =</p>
+        <p>First 40 hours pay =</p>
+        <p>Next hours pay (overtime) = </p>
+        <p>Gross pay = </p>
+        <p>Tax rate for dependents =</p>
+        <p>Tax deduction =</p>
+        <p>Net pay =</p>
+        <p>This employee earned:</p>
+      </div>
+      <div id="results">
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+        <p>Filler</p>
+      </div>
+      <br>
+    </section>
+  </body>
+</html>
